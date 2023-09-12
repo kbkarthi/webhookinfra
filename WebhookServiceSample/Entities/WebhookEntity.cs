5d154141237cc336e8da515c3f6e45ad7a987629
kbkarthi <19604359+kbkarthi@users.noreply.github.com>
2023-09-12 12:38:02 -0700

    Update WebhookEntity.cs

--- WebhookServiceSample/Entities/WebhookEntity.cs
+++ WebhookServiceSample/Entities/WebhookEntity.cs
@@ -1,10 +1,16 @@
-﻿namespace SampleWebApiAspNetCore.Entities
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SampleWebApiAspNetCore.Entities
 {
     public class WebhookEntity
     {
         public int Id { get; set; }
         public string CallBackUrl { get; set; }
         public string WebhookType { get; set; }
+        public string Pivot { get; set; }
+
+        [NotMapped]
+        public IDictionary<string, string> PivotParsed {  get; set; }
         public DateTime Created { get; set; }
     }
 }
