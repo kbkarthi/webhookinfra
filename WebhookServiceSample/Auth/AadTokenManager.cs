d53b2ce62b973fd36dc434cd6db019dba316047c
Pavel Tatarankau <patatara@microsoft.com>
2023-09-12 15:08:29 -0700

    aad auth token

---
+++ WebhookServiceSample/Auth/AadTokenManager.cs
@@ -0,0 +1,39 @@
+﻿namespace WebhookServiceSample.Auth
+{
+    using Microsoft.Identity.Client;
+    using System.Threading.Tasks;
+
+    public class AadTokenManager
+    {
+        private string ClientId = "";
+        private string ClientSecret = "";
+        private string TenantId = "";
+
+        private IConfidentialClientApplication AppInstance;
+
+        private static AadTokenManager TokenManagerInstance;
+
+        public static AadTokenManager GetInstance()
+        {
+            if (TokenManagerInstance == null)
+            {
+                TokenManagerInstance = new AadTokenManager();
+            }
+
+            return TokenManagerInstance;
+        }
+
+        private AadTokenManager()
+        {
+            AppInstance = ConfidentialClientApplicationBuilder.Create(ClientId).WithClientSecret(ClientSecret).Build();
+        }
+
+        public async Task<string> GetAadTokenAsync()
+        {
+            var authResult = await AppInstance.AcquireTokenForClient(scopes: new[] { "https://canary.graph.microsoft.com/.default" })
+                               .WithAuthority(AzureCloudInstance.AzurePublic, TenantId)
+                               .ExecuteAsync();
+            return authResult.AccessToken;
+        }
+    }
+}
