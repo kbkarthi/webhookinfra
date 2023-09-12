bdfac5bb1fee8c6d9ef61f3792e1644515b9222e
kbkarthi <19604359+kbkarthi@users.noreply.github.com>
2023-09-12 12:43:33 -0700

    Update WebhookDto.cs

--- WebhookServiceSample/Dtos/WebhookDto.cs
+++ WebhookServiceSample/Dtos/WebhookDto.cs
@@ -1,10 +1,12 @@
-﻿namespace SampleWebApiAspNetCore.Dtos
+namespace SampleWebApiAspNetCore.Dtos
 {
     public class WebhookDto
     {
         public int Id { get; set; }
         public string CallBackUrl { get; set; }
         public string WebhookType { get; set; }
+        public string Pivot { get; set; }
+        public IDictionary<string, string> PivotParsed { get; set; }
         public DateTime Created { get; set; }
     }
 }
