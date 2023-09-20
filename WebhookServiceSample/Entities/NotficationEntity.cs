b16df47c26a3d77f0d669847dde8f9c9dbfd4bb1
Karthik Karur Balu <kabalu@microsoft.com>
2023-09-20 10:33:59 -0700

    Add Graph API notifications

---
+++ WebhookServiceSample/Entities/NotficationEntity.cs
@@ -0,0 +1,9 @@
+namespace SampleWebApiAspNetCore.Entities
+{
+    public class NotificationEntity
+    {
+        public string Id { get; set; }
+
+        public string Resource { get; set; }
+    }
+}
--- WebhookServiceSample/Repositories/WebhookDbContext.cs
+++ WebhookServiceSample/Repositories/WebhookDbContext.cs
@@ -11,5 +11,6 @@ namespace SampleWebApiAspNetCore.Repositories
         }
 
         public DbSet<WebhookEntity> WebhookItems { get; set; } = null!;
+        public DbSet<NotificationEntity> NotificationItems { get; set; } = null!;
     }
 }
